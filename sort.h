f2d89bdfad694639701d076882c148a8b6e4a059
iihaad9 <fahadfawaz5@gmail.com>
2025-12-21 06:58:14 +0000

    Fix sort.h and add shell_sort prototype

--- sort.h
+++ sort.h
@@ -1,17 +1,16 @@
 #ifndef SORT_H
 #define SORT_H
-#include <stdio.h>
+
 #include <stddef.h>
-#include <stdlib.h>
+#include <stdio.h>
 
 /**
  * struct listint_s - Doubly linked list node
  *
  * @n: Integer stored in the node
- * @prev: Pointer to the previous element of the list
- * @next: Pointer to the next element of the list
+ * @prev: Pointer to the previous element
+ * @next: Pointer to the next element
  */
-
 typedef struct listint_s
 {
 	const int n;
@@ -19,16 +18,18 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+/* Print helpers */
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+
+/* Sorting algorithms */
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-void swap(int *xp, int *yp);
-void print_array(const int *array, size_t size);
-void print_list(const listint_t *list);
-void swap1(int arr[], int size, int *p1, int *p2);
-int partition(int arr[], int size, int low, int high);
-void sort(int arr[], int size, int low, int high);
 
+/* Advanced */
+void shell_sort(int *array, size_t size);
 
 #endif /* SORT_H */
+
